49a7cd1761f86b7cd67690e93d1ffa2f64706d7b
Sergio Arias <s-andres140@hotmail.com>
2024-04-15 15:16:39 -0500

    cambie el author por defecto a null

--- api/models.py
+++ api/models.py
@@ -7,6 +7,6 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes",default=1)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes", null=True)
     def __str__(self):
         return self.title
