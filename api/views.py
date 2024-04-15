c6e018d7e9df579c218f4f133f1c94e9e2e2b904
Sergio Arias <s-andres140@hotmail.com>
2024-04-14 22:02:13 -0500

    intentando hacer la autenticacion con jwt

--- api/views.py
+++
@@ -1,8 +0,0 @@
-from .models import Note
-from rest_framework import viewsets, permissions
-from .serializers import NoteSerializer
-
-class NoteViewSet(viewsets.ModelViewSet):
-    queryset = Note.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = NoteSerializer
