19abe63b6aa76a041902a307db3d8c5d6154ab73
Sergio Arias <s-andres140@hotmail.com>
2024-04-17 11:14:56 -0500

    funciona la actualizacion de tokens

--- frontend/src/context/AuthContextProvider.jsx
+++ frontend/src/context/AuthContextProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState} from 'react'
+import { createContext, useEffect, useState} from 'react'
 import { jwtDecode } from "jwt-decode"
 import { useNavigate } from 'react-router-dom'
 import { api } from '../api'
@@ -10,7 +10,7 @@ export const AuthContextProvider = ({children}) => {
   const [user, setUser] = useState(() => (localStorage.getItem('authTokens') ? jwtDecode(localStorage.getItem('authTokens')) : null))
   const [authTokens, setAuthTokens] = useState(() => (localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null))
   
-  
+
   const navigate = useNavigate()
 
   const loginUser = async (e) =>{
@@ -55,6 +55,24 @@ export const AuthContextProvider = ({children}) => {
       
     }
 
+    const updateToken = async () =>{
+      api
+      .post('/api/token/refresh/',{
+        refresh: authTokens.refresh
+      })
+      .then(response =>{
+        if(response.status === 200){
+          localStorage.setItem('authTokens', JSON.stringify(response.data))
+          setAuthTokens(response.data)
+          setUser(jwtDecode(response.data.access))
+        }
+        else{
+          logoutUser()
+        }
+        
+      })
+    }
+
   const contextData = {
     user: user,
     authTokens: authTokens,
@@ -62,6 +80,19 @@ export const AuthContextProvider = ({children}) => {
     registerUser: registerUser,
     logoutUser: logoutUser,
   }
+
+  useEffect(()=>{
+    
+
+    const REFRESH_INTERVAL = 1000 * 60 * 4 // 4 minutes
+    const interval = setInterval(()=>{
+        if(authTokens){
+            updateToken()
+        }
+    }, REFRESH_INTERVAL)
+    return () => clearInterval(interval)
+
+},[authTokens])
 
   
 
