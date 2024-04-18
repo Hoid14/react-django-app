97ea1870c24812a0214b7125c2e443ddcd639515
Sergio Arias <s-andres140@hotmail.com>
2024-04-18 16:15:58 -0500

    agregue estilos al componente form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,5 +1,6 @@
 
 import { LoadingIndicator } from "./LoadingIndicator"
+import "../styles/form.css"
 
 export const Form = ({name, handleSubmit, loading }) => {
     
@@ -8,10 +9,10 @@ export const Form = ({name, handleSubmit, loading }) => {
             <form onSubmit={handleSubmit} className="form-container">
                 <h1>{name}</h1>
                 <input className="form-input" type="text" name="username" placeholder="Enter username"/>
-                <input className="form-input" type="password" name="password" placeholder="enter password"/>
+                <input className="form-input" type="password" name="password" placeholder="Enter password"/>
                 
                 {loading && <LoadingIndicator />}
-                
+
                 <button className="form-button" type="submit">
                     {name}
                 </button>
