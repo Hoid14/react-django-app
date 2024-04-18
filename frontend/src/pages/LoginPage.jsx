041654a145b61f5cc527227878f0c43a273a0321
Sergio Arias <s-andres140@hotmail.com>
2024-04-18 17:31:33 -0500

    actualice loginPage, para que cuando se salga a login cierre sesion

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,13 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { AuthContext } from "../context/AuthContextProvider"
 import { Form } from "../components/Form"
 
 export const LoginPage = () => {
-    const { loginUser, loading } = useContext(AuthContext)
+    const { loginUser, loading, logoutUser } = useContext(AuthContext)
     
+    useEffect(()=>{
+        logoutUser()
+    },[])
 
     return (
         <Form name="Login" handleSubmit={loginUser} loading={loading}/>
